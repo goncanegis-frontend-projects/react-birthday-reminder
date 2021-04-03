fa272b2f1043099ec467d91107d5ca3876cdeae9
Gonca <gonca.negis@gmail.com>
2021-04-03 23:44:32 +0300

    Add files via upload

---
+++ build/static/js/main.6516fa50.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreminder=this.webpackJsonpreminder||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var r=t(0),c=t.n(r),a=t(2),o=t.n(a);t(8);var u=function(){return c.a.createElement("h2",null,"reminder project setup")};o.a.render(c.a.createElement(c.a.StrictMode,null,c.a.createElement(u,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.6516fa50.chunk.js.map
